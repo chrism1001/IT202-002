be1c5989ddff1d37b2d89656d24af04fcbc5a740
chrism1001 <christophermejia111@gmail.com>
2022-04-17 22:06:15 -0400

    users can now see their cart

--- public_html/Project/api/get_cart.php
+++ public_html/Project/api/get_cart.php
@@ -5,3 +5,31 @@ if (session_status() != PHP_SESSION_ACTIVE) {
     session_start();
 }
 $user_id = get_user_id();
+
+$response = ["status" => 400, "message" => "Unhandled error"];
+http_response_code(400);
+if ($user_id > 0) {
+    $db = getDB();
+    $stmt = $sb->prepare("Select name, c.id as line_id, product_id, desired_quantity, cost, (cost*desired_quantity) as subtotal FROM CART c JOIN Products i on c.product_prid = i.id WHERE c.user_id = :uid");
+    try {
+        $stmt->execute([":uid" => $user_id]);
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        http_response_code(200);
+        if ($results) {
+            $response["status"] = 200;
+            $response["message"] = "Retrieved cart";
+            $response["data"] = $results;
+        } else {
+            $response["status"] = 200;
+            $response["message"] = "No items in cart";
+            $response["data"] = [];
+        }
+    } catch (PDOException $e) {
+        error_log("Error fetching cart" . var_export($e, true));
+    }
+} else {
+    $response["status"] = 403;
+    $response["message"] = "You must be logged in to fetch your cart";
+    http_response_code(403);
+}
+echo json_encode($response);
--- public_html/Project/cart.php
+++ public_html/Project/cart.php
@@ -2,18 +2,49 @@
 require(__DIR__ . "/../../partials/nav.php");
 is_logged_in(true);
 
-$TABLE_NAME = "CART";
-$result = [];
-$columns = get_columns($TABLE_NAME);
-$ignore = ["id", "modified", "created"];
-$db = getDB();
+error_log("get_cart received data: " . var_export($_REQUEST, true));
+if (session_status() != PHP_SESSION_ACTIVE) {
+    session_start();
+}
+$user_id = get_user_id();
 
+$response = ["status" => 400, "message" => "Unhandled error"];
+$res = [];
+//http_response_code(400);
+if ($user_id > 0) {
+    $db = getDB();
+    $stmt = $db->prepare("Select p.name, c.id as line_id, c.product_id, c.desired_quantity, c.unit_price, (c.unit_price*c.desired_quantity) as subtotal FROM CART c JOIN Products p on c.product_id = p.id WHERE c.user_id = :uid");
+    try {
+        $stmt->execute([":uid" => $user_id]);
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        //http_response_code(200);
+        if ($results) {
+            $response["status"] = 200;
+            //flash("Retrieved cart.");
+            //$response["message"] = "Retrieved cart";
+            $response["data"] = $results;
+            $res += $results;
+        } else {
+            $response["status"] = 200;
+            //flash("No items in cart");
+            $response["message"] = "No items in cart";
+            $response["data"] = [];
+        }
+    } catch (PDOException $e) {
+        error_log("Error fetching cart" . var_export($e, true));
+    }
+} else {
+    $response["status"] = 403;
+    $response["message"] = "You must be logged in to fetch your cart";
+    //http_response_code(403);
+}
+//echo json_encode($response);
 
 
 ?>
 
 <script>
-    // get_cart();
+    //get_cart();
 </script>
 
 <div class="container-fluid">
@@ -24,10 +55,24 @@ $db = getDB();
             <th>Quantity</th>
             <th>Cost</th>
             <th>Subtotal</th>
-            <th>Item</th>
+            <th></th>
         </thead>
         <tbody>
-
+            <?php if (count($res) == 0) : ?>
+                <p>Cart is empty</p>
+            <?php else : ?>
+                <?php foreach ($res as $key => $value) : ?>
+                    <tr>
+                        <td><?php se($value["name"]); ?></td>
+                        <td><?php se($value["desired_quantity"]); ?></td>
+                        <td><?php se($value["unit_price"]); ?></td>
+                        <td><?php se($value["subtotal"]); ?></td>
+                        <td>
+                            <button class="btn btn-danger" onclick="deleteLineItem('<?php se($value['line_id']); ?>')">x</button>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </tbody>
     </table>
 </div>

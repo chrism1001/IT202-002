09e48dcaf19f8262c4f97788b825c3e1b3ab9a52
chrism1001 <christophermejia111@gmail.com>
2022-04-29 22:09:27 -0400

    users can now see their purchase history

--- public_html/Project/purchase_history.php
+++ public_html/Project/purchase_history.php
@@ -6,12 +6,12 @@ $results = [];
 $user_id = get_user_id();
 if ($user_id > 0) {
     $db = getDB();
-    $stmt = $db->prepare("SELECT id, name, address, payment_method, total_price, created from Orders WHERE user_id = :uid");
+    $stmt = $db->prepare("SELECT id, name, address, payment_method, total_price, created from Orders WHERE user_id = :uid limit 10");
     try {
         $stmt->execute([":uid" => $user_id]);
         $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
         if ($r) {
-            $results = $r;
+            $results += $r;
         }
     } catch (PDOException $e) {
         error_log(var_export($e, true));
@@ -33,9 +33,18 @@ if ($user_id > 0) {
                         <?php foreach ($record as $column => $value) : ?>
                             <th><?php se($column); ?></th>
                         <?php endforeach; ?>
-                        <th>more info</th>
+                        <th>Actions</th>
                     </thead>
                 <?php endif; ?>
+                    <tr>
+                        <?php foreach ($record as $column => $value) : ?>
+                            <td><?php se($value) ?></td>
+                        <?php endforeach; ?>
+
+                        <td>
+                            <a href="order_information.php?id=<?php se($record, "id"); ?>">More Info</a>
+                        </td>
+                    </tr>
             <?php endforeach; ?>
         </table>
     <?php endif; ?>

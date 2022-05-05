00cb2f4f184bdc17ba3dfb5b238b49c57046cf80
chrism1001 <christophermejia111@gmail.com>
2022-05-05 16:58:51 -0400

    creating ratings table

---
+++ public_html/Project/sql/015_create_table_ratings.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `Ratings` (
+    `id` int AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT,
+    `product_id` INT,
+    `rating` TINYINT(1) DEFAULT 0,
+    `comment` TEXT,
+    `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `modified` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (product_id) REFERENCES Products(id),
+    UNIQUE KEY (user_id, product_id),
+    check (rating>=0 AND rating<=5)
+)

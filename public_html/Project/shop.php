86d3058293f1e2e60bc56f4016fd531fe26f2f64
chrism1001 <christophermejia111@gmail.com>
2022-05-05 17:31:36 -0400

    adding pagination to shop page

--- public_html/Project/shop.php
+++ public_html/Project/shop.php
@@ -19,7 +19,10 @@ if (!in_array($order, ["asc", "desc"])) {
 
 $name = se($_GET, "name", "", false);
 
-$query = "SELECT id, name, description, unit_price, stock FROM $TABLE_NAME WHERE 1=1 and stock > 0 and visibility = 1";
+$base_query = "SELECT id, name, description, unit_price, stock FROM $TABLE_NAME";
+$total_query = "SELECT count(1) as total FROM $TABLE_NAME";
+$query = " WHERE 1=1 and stock > 0";
+
 $params = [];
 if (!empty($name)) {
     $query .= " and name like :name";
@@ -30,17 +33,42 @@ if (!empty($col) && !empty($order)) {
     $query .= " ORDER BY $col $order";
 }
 
-$stmt = $db->prepare($query);
+$stmt = $db->prepare($total_query . $query);
+$total = 0;
 try {
     $stmt->execute($params);
+    $r = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($r) {
+        $total = (int)se($r, "total", 0, false);
+    }
+} catch (PDOException $e) {
+    flash("<pre>" . var_export($e, true) . "</pre>");
+}
+
+$page = se($_GET, "page", 1, false);
+$per_page = 10;
+$offset = ($page - 1) * $per_page;
+$query .= " LIMIT :offset, :count";
+$params[":offset"] = $offset;
+$params[":count"] = $per_page;
+
+$stmt = $db->prepare($base_query . $query);
+foreach ($params as $key => $value) {
+    $type = is_int($value) ? PDO::PARAM_INT : PDO::PARAM_STR;
+    $stmt->bindValue($key, $value, $type);
+}
+$params = null;
+
+try {
+    $stmt->execute($params); //dynamically populated params to bind
     $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
     if ($r) {
         $results = $r;
     }
 } catch (PDOException $e) {
-    error_log(var_export($e, true));
-    flash("Error fetching items", "danger");
+    flash("<pre>" . var_export($e, true) . "</pre>");
 }
+
 ?>
 
 <script>
